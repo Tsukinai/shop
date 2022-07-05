0f3496dcfb9054f3e43ee3394c6406c09360a608
Tsukinai <909436488@qq.com>
2022-07-05 22:55:33 +0800

    新建用户服务接口的实现类

--- src/main/java/cn/edu/hit/service/impl/UserServiceImpl.java
+++ src/main/java/cn/edu/hit/service/impl/UserServiceImpl.java
@@ -1,9 +1,9 @@
-package service.impl;
+package cn.edu.hit.service.impl;
 
-import dao.UserDao;
+import cn.edu.hit.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import service.UserService;
+import cn.edu.hit.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
