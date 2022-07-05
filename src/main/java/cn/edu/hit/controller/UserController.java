3cbfdfe00dcd54896a5f8212643f8eabe1e67744
Tsukinai <909436488@qq.com>
2022-07-05 22:54:26 +0800

    初始化用户控制器

--- src/main/java/cn/edu/hit/controller/UserController.java
+++ src/main/java/cn/edu/hit/controller/UserController.java
@@ -1,10 +1,9 @@
-package controller;
+package cn.edu.hit.controller;
 
-import dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import service.UserService;
+import cn.edu.hit.service.UserService;
 
 @Controller
 @RequestMapping("/user")
