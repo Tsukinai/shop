7ef12fcf7430e70768802c5c055515c349bb56e8
Tsukinai <909436488@qq.com>
2022-07-06 09:35:01 +0800

    用户服务接口

--- src/main/java/cn/edu/hit/service/UserService.java
+++ src/main/java/cn/edu/hit/service/UserService.java
@@ -1,5 +1,14 @@
 package cn.edu.hit.service;
 
+import cn.edu.hit.po.User;
+import org.apache.ibatis.annotations.Select;
+
 public interface UserService {
     void test();
+
+    @Select("select count(0) from `user` where username=#{name}")
+    int changeName(String name);
+
+
+    void addUser(User user);
 }
