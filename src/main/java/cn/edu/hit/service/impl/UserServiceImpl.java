093e6dbef28cf478d276d63367fa8c60f66a098e
Tsukinai <909436488@qq.com>
2022-07-06 09:35:10 +0800

    用户服务接口实现

--- src/main/java/cn/edu/hit/service/impl/UserServiceImpl.java
+++ src/main/java/cn/edu/hit/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.edu.hit.service.impl;
 
 import cn.edu.hit.dao.UserDao;
+import cn.edu.hit.po.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import cn.edu.hit.service.UserService;
@@ -12,6 +13,16 @@ public class UserServiceImpl implements UserService {
     @Override
     public void test(){
 
+    }
+
+    @Override
+    public int changeName(String name) {
+        return userDao.changeName(name);
+    }
+
+    @Override
+    public void addUser(User user) {
+        userDao.addUser(user);
     }
 
 }
